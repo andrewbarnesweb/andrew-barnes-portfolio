0abdd2b24314a750630f2c14047a34660d479a23
Andy Barnes <andy.barnes@emishealth.com>
2020-02-23 10:26:21 +0000

    Added logo as inline SVG

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,6 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Andrew Barnes' Portfolio</h1>
   </Layout>
 )
 
