c6c83c9e0155f558ac5298c597fa10f7c478b65e
Andy Barnes <andy.barnes@emishealth.com>
2020-02-25 19:03:53 +0000

    Included an about section
    About section includes some text (lorem ipsum for now) and some name value pairs with icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react"
 
 import Layout from "../components/layout"
 import Intro from "../components/intro/intro"
+import About from "../components/about/about"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Intro />
+    <About />
   </Layout>
 )
 
