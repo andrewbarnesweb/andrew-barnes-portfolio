7f0c7dd3a1278f102e23a49a02c958db04cbb2c0
Andy Barnes <andy.barnes@emishealth.com>
2020-03-02 10:22:46 +0000

    Consolidated the different areas into a Section component

--- src/components/my-work/my-work.js
+++ src/components/my-work/my-work.js
@@ -1,25 +1,10 @@
 import React from "react"
 import style from "./my-work.module.scss"
+import Section from "../section/section"
 import { StaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
-import VisibilitySensor from 'react-visibility-sensor';
 
 class MyWork extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      visible: false,
-    }
-  }
-
-  onChange(isVisible) {
-    if (isVisible) {
-      this.setState({
-        visible: true
-      });
-    }
-  }
-
   render() {
     return (
       <StaticQuery
@@ -27,7 +12,7 @@ class MyWork extends React.Component {
           query {
             rfs: file(relativePath: { eq: "my-work/rfs.png" }) {
               childImageSharp {
-                fluid(maxWidth: 1000) {
+                fluid(maxWidth: 500, quality: 100) {
                   ...GatsbyImageSharpFluid
                 }
               }
@@ -36,35 +21,31 @@ class MyWork extends React.Component {
         `}
         render={data => (
           <>
-            <VisibilitySensor partialVisibility={true} onChange={this.onChange.bind(this)}>
-              <div className={this.state.visible ? style.containerVisible : style.container}>
-                <h2 className={style.title}>My work</h2>
-
-                <div className={style.work}>
-                  <div className={style.details}>
-                    <h3>Russell Financial Solutions</h3>
-                    <h4>Web design and development</h4>
-                    <time datetime="2020">2020</time>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                  </div>
-                  <div className={style.image}>
-                    <Img fluid={data.rfs.childImageSharp.fluid} />
-                  </div>
+            <Section title="My work">
+              <div className={style.work}>
+                <div className={style.details}>
+                  <h3>Russell Financial Solutions</h3>
+                  <h4>Web design and development</h4>
+                  <time dateTime="2020">2020</time>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
-
-                <div className={style.work}>
-                  <div className={style.detailsAlternate}>
-                    <h3>Russell Financial Solutions</h3>
-                    <h4>Web design and development</h4>
-                    <time datetime="2020">2020</time>
-                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-                  </div>
-                  <div className={style.imageAlternate}>
-                    <Img fluid={data.rfs.childImageSharp.fluid} />
-                  </div>
+                <div className={style.image}>
+                  <Img fluid={data.rfs.childImageSharp.fluid} />
                 </div>
               </div>
-            </VisibilitySensor>
+
+              <div className={style.work}>
+                <div className={style.detailsAlternate}>
+                  <h3>Russell Financial Solutions</h3>
+                  <h4>Web design and development</h4>
+                  <time dateTime="2020">2020</time>
+                  <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+                </div>
+                <div className={style.imageAlternate}>
+                  <Img fluid={data.rfs.childImageSharp.fluid} />
+                </div>
+              </div>
+            </Section>
           </>
         )}
       />
--- src/components/what-i-do/what-i-do.js
+++ src/components/what-i-do/what-i-do.js
@@ -1,5 +1,6 @@
 import React from "react"
 import style from "./what-i-do.module.scss"
+import Section from "../section/section"
 import SkillCard from "./skill-card"
 import Progress from "./progress"
 import VisibilitySensor from 'react-visibility-sensor';
@@ -9,7 +10,6 @@ class WhatIDo extends React.Component {
     super(props)
     this.state = {
       visible: false,
-      blockVisible: false
     }
   }
 
@@ -21,61 +21,50 @@ class WhatIDo extends React.Component {
     }
   }
 
-  onBlockChange(isVisible) {
-    if (isVisible) {
-      this.setState({
-        blockVisible: true
-      });
-    }
-  }
-
   render() {
     return (
-      <VisibilitySensor partialVisibility={true} onChange={this.onBlockChange.bind(this)}>
-        <div className={this.state.blockVisible ? style.containerVisible : style.container}>
-          <h2 className={style.title}>What I do</h2>
-          <div className={style.skills}>
-            <div className={style.skillsContainer}>
-              <SkillCard icon="code" skill="Web development">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Quis nostrud exercitation.
-                </p>
-              </SkillCard>
-            </div>
-            <div className={style.skillsContainer}>
-              <SkillCard icon="design" skill="UI design">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Quis nostrud exercitation.
-                </p>
-              </SkillCard>
-            </div>
+      <Section title="What I do">
+        <div className={style.skills}>
+          <div className={style.skillsContainer}>
+            <SkillCard icon="code" skill="Web development">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Quis nostrud exercitation.
+              </p>
+            </SkillCard>
           </div>
-
-          <div className={style.info}>
-            <div className={style.text}>
-              <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</strong></p>
-              <p>Ut enim ad minim veniam, quis nostrud exercitation. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
-            </div>
-            <div className={style.charts}>
-              <h3 className={style.skillTitle}>Skill one</h3>
-              <VisibilitySensor partialVisibility={true} onChange={this.onChange.bind(this)}>
-                <Progress initial="0%" actual="80%" start={this.state.visible} />
-              </VisibilitySensor>
-              <h3 className={style.skillTitle}>Skill two</h3>
-              <Progress initial="0%" actual="90%" start={this.state.visible} />
-
-              <h3 className={style.skillTitle}>Skill three</h3>
-              <Progress initial="0%" actual="75%" start={this.state.visible} />
-
-              <h3 className={style.skillTitle}>Skill four</h3>
-              <Progress initial="0%" actual="85%" start={this.state.visible} />
-
-              <h3 className={style.skillTitle}>Social skills</h3>
-              <Progress initial="0%" actual="95%" start={this.state.visible} />
-            </div>
+          <div className={style.skillsContainer}>
+            <SkillCard icon="design" skill="UI design">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Quis nostrud exercitation.
+              </p>
+            </SkillCard>
           </div>
         </div>
-      </VisibilitySensor>
+
+        <div className={style.info}>
+          <div className={style.text}>
+            <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</strong></p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
+          </div>
+          <div className={style.charts}>
+            <h3 className={style.skillTitle}>Skill one</h3>
+            <VisibilitySensor partialVisibility={true} onChange={this.onChange.bind(this)}>
+              <Progress initial="0%" actual="80%" start={this.state.visible} />
+            </VisibilitySensor>
+            <h3 className={style.skillTitle}>Skill two</h3>
+            <Progress initial="0%" actual="90%" start={this.state.visible} />
+
+            <h3 className={style.skillTitle}>Skill three</h3>
+            <Progress initial="0%" actual="75%" start={this.state.visible} />
+
+            <h3 className={style.skillTitle}>Skill four</h3>
+            <Progress initial="0%" actual="85%" start={this.state.visible} />
+
+            <h3 className={style.skillTitle}>Social skills</h3>
+            <Progress initial="0%" actual="95%" start={this.state.visible} />
+          </div>
+        </div>
+      </Section>
     );
   }
 }
