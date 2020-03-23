31ecd35d98bcf6daebfb0e0b18cc137494ec81f9
Andy Barnes <andy.barnes@emishealth.com>
2020-03-23 15:42:58 +0000

    Updated content

--- src/components/my-work/my-work.js
+++ src/components/my-work/my-work.js
@@ -9,9 +9,23 @@ class MyWork extends React.Component {
       <StaticQuery
         query={graphql`
           query {
-            rfs: file(relativePath: { eq: "my-work/rfs.png" }) {
+            rfs: file(relativePath: { eq: "my-work/rfs.jpg" }) {
               childImageSharp {
-                fluid(maxWidth: 500, quality: 100) {
+                fluid(maxWidth: 500, quality: 80) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            playCricket: file(relativePath: { eq: "my-work/play-cricket.png" }) {
+              childImageSharp {
+                fluid(maxWidth: 500, quality: 80) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            economist: file(relativePath: { eq: "my-work/economist.jpg" }) {
+              childImageSharp {
+                fluid(maxWidth: 500, quality: 80) {
                   ...GatsbyImageSharpFluid
                 }
               }
@@ -31,18 +45,18 @@ class MyWork extends React.Component {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               </Work>
               <Work
-                title="The Economist"
+                title="Play Cricket Concept"
                 type="Web design and development"
-                image={data.rfs.childImageSharp.fluid}
+                image={data.playCricket.childImageSharp.fluid}
                 year="2020"
                 alternate={true}
               >
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               </Work>
               <Work
-                title="Play Cricket Concept"
+                title="The Economist"
                 type="Web design and development"
-                image={data.rfs.childImageSharp.fluid}
+                image={data.economist.childImageSharp.fluid}
                 year="2020"
               >
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
