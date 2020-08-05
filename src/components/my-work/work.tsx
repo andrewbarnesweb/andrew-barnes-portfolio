7b3aaf0ba2434d5da496fb64f3354f02a8a78f1a
andybarnesemis <andy.barnes@emishealth.com>
2020-08-05 14:48:31 +0100

    Converted a few sections to function components

--- src/components/my-work/work.tsx
+++ src/components/my-work/work.tsx
@@ -3,50 +3,55 @@ import Img from "gatsby-image";
 import PropTypes from "prop-types";
 import style from "./work.module.scss";
 
-const Work = ({ alternate, title, type, year, children, skills, image }) => (
-  <div className={style.work}>
-    <div className={alternate ? style.detailsAlternate : style.details}>
-      <h3>{title}</h3>
-      <h4>{type}</h4>
-      <time className={style.time} dateTime={year}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-        >
-          <path d="M13.333,13.333H10.667V10.667h2.667Zm-4-6.667H6.667V9.333H9.333Zm4,0H10.667V9.333h2.667Zm-8,4H2.667v2.667H5.333Zm4,0H6.667v2.667H9.333Zm-4-4H2.667V9.333H5.333ZM16,1.333V16H0V1.333H2V2A1.333,1.333,0,0,0,4.667,2V1.333h6.667V2A1.333,1.333,0,1,0,14,2V1.333Zm-1.333,4H1.333v9.333H14.667ZM13.333.667A.667.667,0,0,0,12,.667V2a.667.667,0,0,0,1.333,0ZM4,2A.667.667,0,1,1,2.667,2V.667A.667.667,0,1,1,4,.667Z" />
-        </svg>
-        {year}
-      </time>
-      {children}
-      <div className={style.skills}>{skills}</div>
-    </div>
-    <div className={alternate ? style.imageAlternate : style.image}>
-      <Img fluid={image} />
-    </div>
-  </div>
-);
+interface FluidObject {
+  aspectRatio: number;
+  src: string;
+  srcSet: string;
+  sizes: string;
+  base64?: string;
+  tracedSVG?: string;
+  srcWebp?: string;
+  srcSetWebp?: string;
+}
 
-Work.propTypes = {
-  alternate: PropTypes.bool,
-  title: PropTypes.string,
-  type: PropTypes.string,
-  year: PropTypes.string,
-  skills: PropTypes.string,
-  image: PropTypes.objectOf(PropTypes.object),
-  children: PropTypes.node,
-};
+export interface IWorkProps {
+  alternate: boolean;
+  title: string;
+  type: string;
+  year: string;
+  skills: string;
+  image: FluidObject;
+  children: React.ReactNode;
+}
 
-Work.defaultProps = {
-  alternate: false,
-  title: "",
-  type: "",
-  year: "",
-  skills: "",
-  image: null,
-  children: null,
+const Work: React.FC<IWorkProps> = (props: IWorkProps): JSX.Element => {
+  const { alternate, title, type, year, skills, image, children } = props;
+
+  return (
+    <div className={style.work}>
+      <div className={alternate ? style.detailsAlternate : style.details}>
+        <h3>{title}</h3>
+        <h4>{type}</h4>
+        <time className={style.time} dateTime={year}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+          >
+            <path d="M13.333,13.333H10.667V10.667h2.667Zm-4-6.667H6.667V9.333H9.333Zm4,0H10.667V9.333h2.667Zm-8,4H2.667v2.667H5.333Zm4,0H6.667v2.667H9.333Zm-4-4H2.667V9.333H5.333ZM16,1.333V16H0V1.333H2V2A1.333,1.333,0,0,0,4.667,2V1.333h6.667V2A1.333,1.333,0,1,0,14,2V1.333Zm-1.333,4H1.333v9.333H14.667ZM13.333.667A.667.667,0,0,0,12,.667V2a.667.667,0,0,0,1.333,0ZM4,2A.667.667,0,1,1,2.667,2V.667A.667.667,0,1,1,4,.667Z" />
+          </svg>
+          {year}
+        </time>
+        {children}
+        <div className={style.skills}>{skills}</div>
+      </div>
+      <div className={alternate ? style.imageAlternate : style.image}>
+        <Img fluid={image} />
+      </div>
+    </div>
+  );
 };
 
 export default Work;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,28 +9,26 @@ import Blog from "../components/blog/blog";
 import Contact from "../components/contact/contact";
 import SEO from "../components/seo";
 
-const IndexPage: React.FC = (): JSX.Element => {
-  return (
-    <Layout>
-      <SEO title="Andrew Barnes | Front End Web Developer &amp; UI/UX Designer" />
-      <Intro />
-      <section id="about-me">
-        <About />
-      </section>
-      <section id="what-i-do">
-        <WhatIDo />
-      </section>
-      <section id="blog">
-        <Blog />
-      </section>
-      <section id="my-work">
-        <MyWork />
-      </section>
-      <section id="contact">
-        <Contact />
-      </section>
-    </Layout>
-  );
-};
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Andrew Barnes | Front End Web Developer &amp; UI/UX Designer" />
+    <Intro />
+    <section id="about-me">
+      <About />
+    </section>
+    <section id="what-i-do">
+      <WhatIDo />
+    </section>
+    <section id="blog">
+      <Blog />
+    </section>
+    <section id="my-work">
+      <MyWork />
+    </section>
+    <section id="contact">
+      <Contact />
+    </section>
+  </Layout>
+);
 
 export default IndexPage;
