725dccfcb0f873c80c6414f677f7ce4f2f5f3dd1
Andy Barnes <andrew.barnes102@googlemail.com>
2021-05-14 07:21:30 +0100

    Added basic JSON API thing as POC

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,22 @@
  */
 
 // You can delete this file if you're not using it
+const fetch = require(`node-fetch`);
+exports.sourceNodes = async ({ actions: { createNode }, createContentDigest }) => {
+  // get data from GitHub API at build time
+  const result = await fetch(`https://dev.to/api/articles?username=benmatselby`);
+  const resultData = await result.json();
+  // create node for build time data example in the docs
+  createNode({
+    // nameWithOwner and url are arbitrary fields from the data
+    title: resultData,
+    // required fields
+    id: `example-build-time-data`,
+    parent: null,
+    children: [],
+    internal: {
+      type: `Example`,
+      contentDigest: createContentDigest(resultData),
+    },
+  });
+};
