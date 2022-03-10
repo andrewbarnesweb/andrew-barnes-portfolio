73ec9766668ff7634b0a5f69f77d85338daf815a
Andy Barnes <andy@andys-mbp.home>
2022-03-10 08:55:07 +0000

    Bumped deps

--- jest-preprocess.js
+++ jest-preprocess.js
@@ -1,5 +1,8 @@
+const babelJestMd = require('babel-jest');
+const babelJest = babelJestMd.__esModule ? babelJestMd.default : babelJestMd;
+
 const babelOptions = {
   presets: ["babel-preset-gatsby", "@babel/preset-typescript"],
 };
 // eslint-disable-next-line import/no-extraneous-dependencies
-module.exports = require("babel-jest").createTransformer(babelOptions);
+module.exports = babelJest.createTransformer(babelOptions);
--- jest.config.js
+++ jest.config.js
@@ -21,4 +21,5 @@ module.exports = {
       statements: 100,
     },
   },
+  testEnvironment: "jsdom"
 };
